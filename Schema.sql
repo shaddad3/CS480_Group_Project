001fc7336f76b7be58f0cb96e184d412aed1c6e3
Hrushikesh Joshi <hjosh@uic.edu>
2024-11-29 05:56:43 -0600

    changed Schema.sql and Data.sql

--- Schema.sql
+++ Schema.sql
@@ -1,83 +1,79 @@
+DROP DATABASE IF EXISTS course_registration_website;
+
 CREATE DATABASE IF NOT EXISTS course_registration_website;
 
 USE course_registration_website;
 
 CREATE TABLE Administrator (
-    UIN INT PRIMARY KEY,
-    name VARCHAR(100),
-    email VARCHAR(100),
-    used_id VARCHAR(100) UNIQUE,
-    password VARCHAR(100)
+    administrator_id INT PRIMARY KEY AUTO_INCREMENT,
+    administrator_username VARCHAR(50) UNIQUE,
+    administrator_password VARCHAR(255),
+    administrator_first_name VARCHAR(50),
+    administrator_last_name VARCHAR(50),
+    administrator_email VARCHAR(320)
 );
 
 CREATE TABLE Department (
-    dept_id INT PRIMARY KEY,
-    dept_name VARCHAR(100) UNIQUE,
-    dept_head VARCHAR(100)
+    department_id INT PRIMARY KEY AUTO_INCREMENT,
+    department_name VARCHAR(100) UNIQUE,
+    department_head_first_name VARCHAR(50),
+    department_head_last_name VARCHAR(50)
 );
 
 CREATE TABLE Instructor (
-    UIN INT PRIMARY KEY,
-    name VARCHAR(100),
-    email VARCHAR(100),
-    dept_name VARCHAR(100) NOT NULL,
-    course_id VARCHAR(100),
-    admin_id INT NOT NULL,
-    FOREIGN KEY (admin_id) REFERENCES Administrator (UIN) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (dept_name) REFERENCES Department (dept_name) ON DELETE CASCADE ON UPDATE CASCADE
+    instructor_id INT PRIMARY KEY AUTO_INCREMENT,
+    instructor_username VARCHAR(50) UNIQUE,
+    instructor_password VARCHAR(255),
+    instructor_first_name VARCHAR(100),
+    instructor_last_name VARCHAR(50),
+    instructor_email VARCHAR(320),
+    administrator_id INT,
+    department_id INT,
+    FOREIGN KEY (administrator_id) REFERENCES Administrator (administrator_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (department_id) REFERENCES Department (department_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE Courses (
-    course_id VARCHAR(100) PRIMARY KEY,
-    course_name VARCHAR(100),
-    credits INT,
-    dept_name VARCHAR(100) NOT NULL,
-    instructor VARCHAR(100),
-    instruction_method VARCHAR(100),
-    admin_id INT NOT NULL,
-    day VARCHAR(100),
-    time VARCHAR(100),
-    location VARCHAR(100),
-    availability INT,
-    prereq_id VARCHAR(100),
-    FOREIGN KEY (prereq_id) REFERENCES Courses (course_id) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (admin_id) REFERENCES Administrator (UIN) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (dept_name) REFERENCES Department (dept_name) ON DELETE CASCADE ON UPDATE CASCADE
+    course_id INT PRIMARY KEY AUTO_INCREMENT,
+    course_name VARCHAR(100) UNIQUE,
+    course_credits INT,
+    course_instruction_method VARCHAR(50),
+    course_lecture_day VARCHAR(10),
+    course_lecture_time VARCHAR(20),
+    course_lecture_location VARCHAR(50),
+    course_available_seats INT,
+    prerequisite_course_id INT,
+    administrator_id INT,
+    department_id INT,
+    FOREIGN KEY (prerequisite_course_id) REFERENCES Courses (course_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (administrator_id) REFERENCES Administrator (administrator_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (department_id) REFERENCES Department (department_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE Teaches (
-    inst_id INT,
-    course_id VARCHAR(100) NOT NULL,
-    PRIMARY KEY (inst_id, course_id),
-    FOREIGN KEY (inst_id) REFERENCES Instructor (UIN) ON DELETE CASCADE ON UPDATE CASCADE,
+    instructor_id INT,
+    course_id INT,
+    PRIMARY KEY (instructor_id, course_id),
+    FOREIGN KEY (instructor_id) REFERENCES Instructor (instructor_id) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (course_id) REFERENCES Courses (course_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE Student (
-    UIN INT PRIMARY KEY,
-    name VARCHAR(100),
-    email VARCHAR(100),
-    user_id VARCHAR(100) UNIQUE,
-    password VARCHAR(100),
-    level VARCHAR(20),
-    admin_id INT NOT NULL,
-    FOREIGN KEY (admin_id) REFERENCES Administrator (UIN) ON DELETE CASCADE ON UPDATE CASCADE
+    student_id INT PRIMARY KEY AUTO_INCREMENT,
+    student_username VARCHAR(50) UNIQUE,
+    student_password VARCHAR(255),
+    student_first_name VARCHAR(50),
+    student_last_name VARCHAR(50),
+    student_email VARCHAR(320),
+    student_level VARCHAR(20),
+    administrator_id INT,
+    FOREIGN KEY (administrator_id) REFERENCES Administrator (administrator_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
--- This table is actually unnecessary as this info is just a duplicate of the Takes relation
--- CREATE TABLE Student_Courses (
---     student_id INT,
---     course_id VARCHAR(50),
---     PRIMARY KEY (student_id, course_id),
---     FOREIGN KEY (student_id) REFERENCES Student (UIN) ON DELETE CASCADE ON UPDATE CASCADE,
---     FOREIGN KEY (course_id) REFERENCES Courses (course_id) ON DELETE CASCADE ON UPDATE CASCADE
--- );
-
 CREATE TABLE Takes (
+    course_id INT,
     student_id INT,
-    course_id VARCHAR(100) NOT NULL,
-    fee_amt INT,
     PRIMARY KEY (student_id, course_id),
-    FOREIGN KEY (student_id) REFERENCES Student (UIN) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (student_id) REFERENCES Student (student_id) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (course_id) REFERENCES Courses (course_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
