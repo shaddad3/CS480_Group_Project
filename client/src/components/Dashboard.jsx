255f5b055e220e439a23dff5f4a9cd2ca4a910b8
Hrushikesh Joshi <hjosh@uic.edu>
2024-12-01 19:32:45 -0600

    saving before merging code

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,5 +1,17 @@
+import { useContext } from "react";
+
+import { Context } from "../Context";
+
 import "./Dashboard.css";
 
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  const { user } = useContext(Context);
+
+  return (
+    <>
+      {user.role === "administrator" && <div>adminstrator</div>}
+      {user.role === "instructor" && <div>Instructor</div>}
+      {user.role === "student" && <div>Student</div>}
+    </>
+  );
 }
